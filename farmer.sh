966b5149440916caec1f49622c27337a5e5ee707
Skedush <candycakechocolate@hotmail.com>
2022-06-04 12:08:25 +0800

    fix(run&&farmer): 版本更新

--- farmer.sh
+++ farmer.sh
@@ -2,4 +2,4 @@
 
 # 将WALLET_ADDRESS修改为自己的钱包地址
 
-nohup ./farmer farm --reward-address WALLET_ADDRESS --plot-size 40G > farmer.log 2>&1 &
+nohup ./farmer farm --reward-address WALLET_ADDRESS --plot-size 50G > farmer.log 2>&1 &
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-02/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-02
+wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03
 mv subspace-node-ubuntu-x86_64-gemini-1b-2022-june-02 node
 
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-02/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-02
+wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03
 mv subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-02 farmer
 chmod +x node.sh farmer.sh node farmer
 ./node.sh
