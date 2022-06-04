5f477d5189b6eb74852fd7bd0b3b5dc81b8f8d16
Skedush <candycakechocolate@hotmail.com>
2022-06-04 12:18:59 +0800

    fix(run): 优化

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03
-mv subspace-node-ubuntu-x86_64-gemini-1b-2022-june-02 node
+wget -0 node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03
 
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03
-mv subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-02 farmer
+wget -0 farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03
 chmod +x node.sh farmer.sh node farmer
 ./node.sh
 sleep 10
