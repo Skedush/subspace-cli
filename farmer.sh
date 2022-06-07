b71831d4420c40e2e682ed58b510d40945673921
Skedush <candycakechocolate@hotmail.com>
2022-06-07 20:18:05 +0800

    fix(update): update

--- farmer.sh
+++ farmer.sh
@@ -2,4 +2,4 @@
 
 # 将WALLET_ADDRESS修改为自己的钱包地址
 
-nohup ./farmer farm --reward-address WA --plot-size 50G > farmer.log 2>&1 &
+nohup ./farmer farm --reward-address WA --plot-size 100G > farmer.log 2>&1 &
